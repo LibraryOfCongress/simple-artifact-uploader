5841e3207f661c14f08896d06fa7e280b493a02a
John Scancella <jsca@loc.gov>
2016-05-10 08:14:47 -0400

    added more logging

--- src/main/java/gov/loc/repository/SimpleArtifactUploaderPlugin.java
+++ src/main/java/gov/loc/repository/SimpleArtifactUploaderPlugin.java
@@ -3,6 +3,8 @@ package gov.loc.repository;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
 import org.gradle.api.tasks.bundling.AbstractArchiveTask;
 
 import gov.loc.repository.extension.UploadPluginExtension;
@@ -12,6 +14,8 @@ import gov.loc.repository.task.UploadTask;
  * The class that extends gradle by adding the artifactory closure and the upload task
  */
 public class SimpleArtifactUploaderPlugin implements Plugin<Project>{
+  private static final Logger logger = Logging.getLogger(SimpleArtifactUploaderPlugin.class);
+  
   @Override
   public void apply(Project project) {
     UploadPluginExtension extension = project.getExtensions().create("artifactory", UploadPluginExtension.class); //define the artifactory closure
@@ -22,6 +26,7 @@ public class SimpleArtifactUploaderPlugin implements Plugin<Project>{
     //make sure tasks that generate artifacts happen first
     for(Task task : project.getTasks()){
       if(task instanceof AbstractArchiveTask){
+        logger.debug("Adding dependsOn {} for uploadToArtifactory", task.getName());
         upload.dependsOn(task);
       }
     }
